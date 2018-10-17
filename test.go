27a1b7108ee737c8eb64e96d401c00af094d39a8
chrisYuYu <essionmao@outlook.com>
2018-10-17 20:28:47 +0800

    提交测试

---
+++ test.go
@@ -0,0 +1,8 @@
+package main
+import ( "github.com/gomodule/redigo/redis")
+func main(){
+	        conn,_ := redis.Dial("tcp", ":6379")
+		        defer conn.Close()
+			        conn.Do("set", "c1", "hello")
+			}
+
